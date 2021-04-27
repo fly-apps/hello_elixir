edc92f73055aec8742d344ad2410efd979f5bd10
Mark Ericksen <brainlid@gmail.com>
2021-04-27 15:35:49 -0600

    add comment for inet6 config

--- config/runtime.exs
+++ config/runtime.exs
@@ -17,6 +17,7 @@ if config_env() == :prod do
     url: [host: "#{app_name}.fly.dev", port: 80],
     http: [
       port: String.to_integer(System.get_env("PORT") || "4000"),
+      # IMPORTANT: support IPv6 addresses
       transport_options: [socket_opts: [:inet6]]
     ],
     secret_key_base: secret_key_base
