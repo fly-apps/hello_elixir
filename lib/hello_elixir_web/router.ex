1696de90a90fcbd1c69fa98689b4af94bb345036
Mark Ericksen <brainlid@gmail.com>
2021-04-28 17:00:06 -0600

    do as a basic phoenix app - not live_view (unused and complicates the example)

--- lib/hello_elixir_web/router.ex
+++ lib/hello_elixir_web/router.ex
@@ -4,8 +4,7 @@ defmodule HelloElixirWeb.Router do
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, {HelloElixirWeb.LayoutView, :root}
+    plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
@@ -17,7 +16,7 @@ defmodule HelloElixirWeb.Router do
   scope "/", HelloElixirWeb do
     pipe_through :browser
 
-    live "/", PageLive, :index
+    get "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.
