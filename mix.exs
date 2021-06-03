169a0d9d7e6fedd6789b666c0be9ca1f970b70e3
Mark Ericksen <brainlid@gmail.com>
2021-06-03 14:41:09 -0600

    update to Elixir 1.12 and OTP 24 - minor update to dependencies

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule HelloElixir.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.7"},
+      {:phoenix, "~> 1.5.8"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
