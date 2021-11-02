9e5734c41fb1cef5f57b95025232d40e21792b56
Mark Ericksen <brainlid@gmail.com>
2021-11-02 17:25:38 -0600

    apply all the changes to make it a 1.6 phx app (#4)
    - uses esbuild
    - removed webpack
    - lots of changes

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,4 @@
-use Mix.Config
+import Config
 
 # Configure your database
 #
@@ -10,13 +10,21 @@ config :hello_elixir, HelloElixir.Repo,
   password: "postgres",
   database: "hello_elixir_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :hello_elixir, HelloElixirWeb.Endpoint,
-  http: [port: 4002],
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "ZSboDtvMYxxeMHqCOlTGvFkIAXNXUONkHC3mt3CE+34iYHClSKlJ1FMxA3/oK8lG",
   server: false
+
+# In test we don't send emails.
+config :hello_elixir, HelloElixir.Mailer, adapter: Swoosh.Adapters.Test
 
 # Print only warnings and errors during test
 config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
